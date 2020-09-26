0d7de82b305924aa6b36707e615f0e9291f96260
J Hill <chirolhi@usc.edu>
2020-09-26 14:48:31 -0700

    Complete

---
+++ .eleventy.js
@@ -0,0 +1,24 @@
+module.exports = (function (eleventyConfig) {
+    eleventyConfig.addFilter("formatted-date", function (date) {
+        if(!(/^([0-9]{1,2}\/){2}[0-9]{2}$/).test(date)){
+            return "Improperly formatted date";
+        }
+
+        let dateComponents = date.split('/');
+
+        const monthNames = [
+            "January", "February", "March", "April", "May", "June",
+            "July", "August", "September", "October", "November", "December"
+        ];
+        return `${monthNames[dateComponents[0] - 1]} ${dateComponents[1]}, 20${dateComponents[2]}`
+    });
+
+    eleventyConfig.addFilter("plus-one", function (num) {
+        return num + 1;
+    });
+
+    eleventyConfig.addPassthroughCopy('css')
+    return {
+        passthroughFileCopy: true
+    }
+});
